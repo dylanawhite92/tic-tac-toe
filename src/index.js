b73a401aea02950f091ec1fc9d5af5f5615c225e
Dylan White <dylanawhite11@gmail.com>
2019-12-05 13:24:08 -0600

    created helper function to loop over array of squares and determine if a winner has been found yet

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+// Class based component
 // class Square extends React.Component {
 //   render() {
 //     return (
@@ -12,6 +13,7 @@ import "./index.css";
 //   }
 // }
 
+// Function based component
 const Square = props => {
   return (
     <button className="square" onClick={props.onClick}>
@@ -20,6 +22,7 @@ const Square = props => {
   );
 };
 
+// Game board
 class Board extends React.Component {
   constructor(props) {
     super(props);
@@ -29,6 +32,7 @@ class Board extends React.Component {
     };
   }
 
+  // Handle click events
   handleClick(i) {
     const squares = this.state.squares.slice();
     squares[i] = this.state.xIsNext ? "X" : "O";
@@ -38,6 +42,7 @@ class Board extends React.Component {
     });
   }
 
+  // Handle rendering squares on board
   renderSquare(i) {
     return (
       <Square
@@ -91,5 +96,29 @@ class Game extends React.Component {
     );
   }
 }
+
+// Helper function to keep track of who will win
+const calculateWinner = squares => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+};
 
 ReactDOM.render(<Game />, document.getElementById("root"));
