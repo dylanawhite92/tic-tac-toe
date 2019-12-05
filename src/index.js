16ef802fbba9773425616be59c1ce0d0e9e78933
Dylan White <dylanawhite11@gmail.com>
2019-12-05 13:09:27 -0600

    updated render for next player

--- src/index.js
+++ src/index.js
@@ -48,7 +48,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next Player: X";
+    const status = `Next Player: ${this.state.xIsNext ? "X" : "O"}`;
 
     return (
       <>
